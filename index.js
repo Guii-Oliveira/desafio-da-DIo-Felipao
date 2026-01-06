7cb1d1b7527b87af2ae1b4a910746a22406d3994
Guii-Oliveira <guioliveira3344@gmail.com>
2026-01-06 19:35:03 -0300

    terceira tentativa

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 let heroi = guerreiro
 let xp = "1000"
+switch(cp){
+    case "1000":
+        console.log("prata")
+        break
+        case"2000":
+        console.log("prata")
+        break
+}
